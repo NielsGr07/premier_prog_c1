97b586d2027a19dd883b1cad25057d41443eede6
NielsGr07 <niels.gruneisen@esme.fr>
2025-09-12 10:38:30 +0200

    Update program.c

--- program.c
+++ program.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("+----------------------------- EFIS DASHBOARD -----------------------------+\n");
+    printf("| Speed (km/h)               Horizon / Turn Indicator             Alt (ft) |\n");
+    printf("| +------------+            +--------------------------+          +------+ |\n");
+    printf("| | 300        |            |          /\\              |          |12000 | |\n");
+    printf("| | 250        |            |         /  \\             |          |      | |\n");
+    printf("| | 200        |            |        /    \\            |          |      | |\n");
+    printf("| | 150        |            |-------|----|-------       |          |      | |\n");
+    printf("| | 100        |            |        \\    /            |          |      | |\n");
+    printf("| |  50        |            |         \\  /             |          |      | |\n");
+    printf("| |   0        |            |          \\/              |          |      | |\n");
+    printf("| +------------+            +---------------------------+          +------+ |\n");
+    printf("|                                                                           |\n");
+    printf("|                                                                           |\n");
+    printf("|                                                                           |\n");
+    printf("|                                                                           |\n");
+    printf("|                                                                           |\n");
+    printf("|                                                                           |\n");
+    printf("|                                                                           |\n");
+    printf("|                                                                           |\n");
+    printf("|                                                                           |\n");
+    printf("|                                                                           |\n");
+    printf("|                                                                           |\n");
+    printf("|                                                                           |\n");
+    printf("|                           Heading: 045° NE                                |\n");
+    printf("+---------------------------------------------------------------------------+\n");
     return 0;
 }
