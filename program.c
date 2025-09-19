92ecc336b987be87155ab5f90f5ed9fc393feb9c
NielsGr07 <niels.gruneisen@esme.fr>
2025-09-19 08:56:46 +0000

    code ajusté et fonctionnel

--- program.c
+++ program.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    printf("+----------------------------- EFIS DASHBOARD -----------------------------+\n");
-    printf("| Speed (km/h)               Horizon / Turn Indicator             Alt (ft) |\n");
-    printf("| +------------+            +--------------------------+          +------+ |\n");
-    printf("| | 300        |            |          /\\              |          |12000 | |\n");
-    printf("| | 250        |            |         /  \\             |          |      | |\n");
-    printf("| | 200        |            |        /    \\            |          |      | |\n");
-    printf("| | 150        |            |-------|------|-----------|          |      | |\n");
-    printf("| | 100        |            |        \\    /            |          |      | |\n");
-    printf("| |  50        |            |         \\  /             |          |      | |\n");
-    printf("| |   0        |            |          \\/              |          |      | |\n");
-    printf("| +------------+            +--------------------------+          +------+ |\n");
+    // Variables dynamiques
+    int speed    = 300;     // en kts
+    int altitude = 10000;   // en ft
+    int pitch    = 5;       // degrés
+    int roll     = -10;     // degrés
+    int heading  = 12;      // degrésA
+    printf("+=============================== EFIS DASHBOARD ============================+\n");
+    printf("| SPD(kts)                                                          ALT(ft) |\n");
+    printf("| +--------+           +----------------------------------+        +------+ |\n");
+    printf("| | %3d    |           |              SKY                 |        |%5d | |\n", speed + 50, altitude + 1000);
+    printf("|>| %3d    |<          | Pitch:%+3d°             Roll:%+3d° |       >|%5d |<|\n", speed, pitch, roll, altitude);
+    printf("| | %3d    |           |   +10°    ----|----    +10°      |        |%5d | |\n", speed - 50, altitude - 1000);
+    printf("| | %3d    |           |    +5°     ---|---     +5°       |        |%5d | |\n", speed - 100, altitude - 2000);
+    printf("| | %3d    |           |------------HORIZON---------------|        |%5d | |\n", speed - 150, altitude - 3000);
+    printf("| | %3d    |           |    -5°     ---|---     -5°       |        |%5d | |\n", speed - 200, altitude - 4000);
+    printf("| | %3d    |           |   -10°    ----|----    -10°      |        |%5d | |\n", speed - 250, altitude - 5000);
+    printf("| +--------+           |             GROUND               |        +------+ |\n");
+    printf("|                      +----------------------------------+                 |\n");
     printf("|                                                                           |\n");
     printf("|                                                                           |\n");
-    printf("|                                                                           |\n");
-    printf("|                                                                           |\n");
-    printf("|                                                                           |\n");
-    printf("|                                                                           |\n");
-    printf("|                                                                           |\n");
-    printf("|                                                                           |\n");
-    printf("|                                                                           |\n");
-    printf("|                                                                           |\n");
-    printf("|                                                                           |\n");
-    printf("|                                                                           |\n");
-    printf("|                           Heading: 045° NE                                |\n");
-    printf("+--------------------------------------------------------------------------+\n");
+    printf("|                             +-----------------+                           |\n");
+    printf("|                             |   NW   N   NE   |                           |\n");
+    printf("|                             |   W    ^    E   |                           |\n");
+    printf("|                             |   SW   S   SE   |                           |\n");
+    printf("|                             +-----------------+                           |\n");
+    printf("|                             Heading: %03d° N                               |\n", heading);
+    printf("+===========================================================================+\n");
+
     return 0;
 }
